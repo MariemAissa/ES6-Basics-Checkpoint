c4c908d05203a1229671d4d88e9a4e5c580855b7
Mariem Aissa <maissa@equalios.com>
2020-03-20 22:59:50 +0100

    es6-update

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const pets = [
-  { name: "Max", type: "dog", bornOn: 2018 },
-  { name: "Angel", type: "cat", bornOn: 2015 },
-  { name: "Jasper", type: "dog", bornOn: 2016 }
+    {name: "Max", type: "dog", bornOn: 2018},
+    {name: "Angel", type: "cat", bornOn: 2015},
+    {name: "Jasper", type: "dog", bornOn: 2016}
 ];
 
-const getAge1=(pet) => {
+const getAge1 = (pet) =>
+{
     return new Date().getFullYear() - pet.bornOn;
 }
 
 //map function
-const petsWithAge=pets.map((pet) => {
-    pet.age=getAge1(pet);
-    return pet;
+const petsWithAge = pets.map(pet => {
+    pet.age = getAge1(pet);
+return pet;
 })
 console.log("petsWithAge", petsWithAge);
 
 
 //Filter Function
-const dogs=pets.filter((pet) => {
-  return pet.type === "dog"
-})
-console.log(dogs);
+const dogs = pets.filter(pet => pet.type === "dog")
+console.log("dogs", dogs);
 
 
 //Find Function
 let jasper;
-jasper=pets.find((pet) =>{
-  return pet.name === "Jasper";
-})
+jasper = pets.find(pet => pet.name === "Jasper");
 console.log(`Jasper is ${jasper.age} years old`);
